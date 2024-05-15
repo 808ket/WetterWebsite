915777cac0950ee315d955540146decc165a49f0
Schueler-ADM <my@mail.de>
2024-05-15 11:50:01 +0200

    database config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "weathersite",
+        connectTimeout: 5000
+    },
+};
+module.exports = config;
