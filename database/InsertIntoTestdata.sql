607d70e080930f791383e4c388c6a886ed18e130
cstein <c.stein93@web.de>
2024-06-02 13:56:00 +0200

    fix testdata

--- database/InsertIntoTestdata.sql
+++ database/InsertIntoTestdata.sql
@@ -2,8 +2,8 @@ USE weathersite;
 
 INSERT INTO WeatherLogs (city, temperature, humidity, description, windspeed, time)
     VALUES ('Wismar', 21.60, 79, 'Ein paar Wolken', 4.98, '2024-06-02 13:06:22'),
-           ('Schwerin', 21.17, 81, 'Mäßig bewölkt', 4.02), '2024-06-02 13:06:48',
-           ('Rostock', 21.60, 78, 'Mäßig bewölkt', 6.60), '2024-06-02 13:07:10',
+           ('Schwerin', 21.17, 81, 'Mäßig bewölkt', 4.02, '2024-06-02 13:06:48'),
+           ('Rostock', 21.60, 78, 'Mäßig bewölkt', 6.60, '2024-06-02 13:07:10'),
            ('Stralsund', 21.35, 76, 'Mäßig bewölkt', 5.14, '2024-06-02 13:07:40'),
            ('Greifswald', 21.71, 90, 'Überwiegend bewölkt', 5.77, '2024-06-02 13:08:24')
             ;
